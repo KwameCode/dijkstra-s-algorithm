93e441d0b5af22257e62cf7e290dd78533decd5c
KwameCode <kwameogh@gmail.com>
2017-04-18 06:23:33 -0500

    Defining constructor

--- weightedGraph.py
+++ weightedGraph.py
@@ -1,1 +1,7 @@
+# A weighted Graph class
 class weightedGraph():
+
+    # Defiing constructor for a weighted graph
+    def _init_(self, vertices, edges):
+        self.vertices = vertices
+        self.edges = edges
